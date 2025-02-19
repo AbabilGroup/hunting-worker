68fdcd999e9173d386b8828352296bad69f96ec0
Masum Billah Zihadi <zihadimasumbillah@gmail.com>
2025-02-19 12:52:36 +0600

    update code

--- src/components/ui/3d-card.test.tsx
+++
@@ -1,8 +0,0 @@
-const { render, screen } = require('@testing-library/react');
-const ThreeDCard = require('./3d-card');
-
-test('hello world!', () => {
-	render(<ThreeDCard />);
-	const element = screen.getByText(/hello world/i);
-	expect(element).toBeInTheDocument();
-});
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import tailwindAnimate from "tailwindcss-animate";
 
 export default {
   darkMode: ["class"],
@@ -66,5 +67,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindAnimate],
 } satisfies Config;
